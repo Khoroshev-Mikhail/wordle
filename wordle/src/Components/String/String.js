161cfeb0e8a196c35fbf98eb1ea6217cec7d07c0
Mikha38 <oyks1379@yandex.ru>
2022-06-17 23:32:06 +0300

    css

--- wordle/src/Components/String/String.js
+++ wordle/src/Components/String/String.js
@@ -6,7 +6,7 @@ export default memo(String);
 function String(props){
     //Рефакторинг? className
     return(
-        <div className="string" style={{border: props.current ? '5px solid black' : 'none'}}>
+        <div className="string" style={{border: props.current ? '1px solid black' : 'none'}}>
             {Array(5).fill().map((_, i) => {
 
                 const trueLetter = props.trueWord ? props.trueWord[i].toLowerCase() : ''
